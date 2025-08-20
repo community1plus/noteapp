1fc41df045d2c7baf7e94d32a3dc2b42bc0ff268
Ade Oloyede <ade-oloyede@hotmail.com>
2025-08-21 03:44:27 +1000

    Consolidate files from original communityplus build

--- components/App.jsx
+++ components/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
-import CommunityPlusLandingPage from '../../communityplus-app/src/components/CommunityPlusLandingPage';
-import CommunityPlusRegistrationPage from '../../communityplus-app/src/components/CommunityPlusRegistrationPage';
+import CommunityPlusLandingPage from '../../notepad/src/components/CommunityPlusLandingPage';
+import CommunityPlusRegistrationPage from '../../notepad/src/components/CommunityPlusRegistrationPage';
 
 function App() {
   return (
