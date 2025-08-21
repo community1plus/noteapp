dbfe210772fd0e5a41cdbae54107e5a3b8e03b51
Ade Oloyede <ade-oloyede@hotmail.com>
2025-08-21 13:27:03 +1000

    Add homepage placeholder

--- components/App.jsx
+++ components/App.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import CommunityPlusLandingPage from './CommunityPlusLandingPage';
 import CommunityPlusRegistrationPage from './CommunityPlusRegistrationPage';
 import CommunityPlusHomePage from './CommunityPlusHomePage';
