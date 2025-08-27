fc632ab6fb65547591c7f7e614877891f6bba86b
Ade Oloyede <ade-oloyede@hotmail.com>
2025-08-27 22:34:59 +1000

    Add Social login with Cognito

--- amplify/auth/resource.ts
+++ amplify/auth/resource.ts
@@ -9,17 +9,7 @@ const GOOGLE_CLIENT_SECRET = secret("GOOGLE_CLIENT_SECRET");
 const FACEBOOK_CLIENT_ID = secret("FACEBOOK_CLIENT_ID");
 const FACEBOOK_CLIENT_SECRET = secret("FACEBOOK_CLIENT_SECRET");
 
-const APPLE_CLIENT_ID = secret("APPLE_CLIENT_ID");
-const APPLE_TEAM_ID = secret("APPLE_TEAM_ID");
-const APPLE_KEY_ID = secret("APPLE_KEY_ID");
-const APPLE_PRIVATE_KEY = secret("APPLE_PRIVATE_KEY");
-
-const AMAZON_CLIENT_ID = secret("AMAZON_CLIENT_ID");
-const AMAZON_CLIENT_SECRET = secret("AMAZON_CLIENT_SECRET");
-
-const TWITTER_CLIENT_ID = secret("TWITTER_CLIENT_ID");
-const TWITTER_CLIENT_SECRET = secret("TWITTER_CLIENT_SECRET");
-
+// Export a default empty auth configuration to satisfy Amplify
 export const auth = defineAuth({
   loginWith: {
     email: true,   // allow email sign-up/login
@@ -41,23 +31,9 @@ export const auth = defineAuth({
       facebook: {
         clientId: FACEBOOK_CLIENT_ID,
         clientSecret: FACEBOOK_CLIENT_SECRET,
-        
+        scopes: ["public_profile", "email"],
       },
-      signInWithApple: {
-        clientId: secret('SIWA_CLIENT_ID'),
-        keyId: secret('SIWA_KEY_ID'),
-        privateKey: secret('SIWA_PRIVATE_KEY'),
-        teamId: secret('SIWA_TEAM_ID')
-      },
-      loginWithAmazon: {
-        clientId: secret('LOGINWITHAMAZON_CLIENT_ID'),
-        clientSecret: secret('LOGINWITHAMAZON_CLIENT_SECRET')
-      },
-    },
+      
+    }, 
   },
-  
 });
-// Note: To use social providers, you must configure them in the respective
-// developer consoles (Google, Facebook, Apple, Amazon, Twitter) and set the
-// callback URLs to point to your Cognito User Pool. See the documentation for
-// more details: https://docs.amplify.aws/cli/auth/social-provider/s/overview/
