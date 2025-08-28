261f2cea6c39f50d80653be847c8af0a74cfd5f5
Ade Oloyede <ade-oloyede@hotmail.com>
2025-08-28 17:58:09 +1000

    Add Social login with Cognito

--- amplify/auth/resource.ts
+++ amplify/auth/resource.ts
@@ -1,8 +1,19 @@
 import { defineAuth } from "@aws-amplify/backend-auth";
+import { secret } from "@aws-amplify/backend";
 
 export const auth = defineAuth({
   loginWith: {
     email: true,
+    externalProviders: {
+      google: {
+        clientId: secret("GOOGLE_CLIENT_ID"),
+        clientSecret: secret("GOOGLE_CLIENT_SECRET"),
+      },
+      facebook: {
+        clientId: secret("FACEBOOK_APP_ID"),
+        clientSecret: secret("FACEBOOK_APP_SECRET"),
+      },
+    },
   },
 });
 
