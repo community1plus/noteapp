a9f9e3819fae49b9b3625cc1cfc8c6bb758f23cf
Ade Oloyede <ade-oloyede@hotmail.com>
2025-09-01 20:19:26 +1000

    Remove Google & Facebook IdPs from Amplify auth

--- src/SignInRegister.jsx
+++ src/SignInRegister.jsx
@@ -1,7 +1,7 @@
 import { Amplify } from 'aws-amplify';
 import { withAuthenticator } from '@aws-amplify/ui-react';
 import '@aws-amplify/ui-react/styles.css';
-import awsExports from './aws-exports';
+
 
 Amplify.configure(awsExports);
 
