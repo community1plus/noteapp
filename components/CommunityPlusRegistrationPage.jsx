625b851dfa42155e9eb5ab13d8a2ebe884d13461
Ade Oloyede <ade-oloyede@hotmail.com>
2025-09-01 18:00:59 +1000

    Remove Google & Facebook IdPs from Amplify auth

--- components/CommunityPlusRegistrationPage.jsx
+++ components/CommunityPlusRegistrationPage.jsx
@@ -14,31 +14,3 @@ export const auth = defineAuth({
 })
 
 
-const SignInRegister = () => {
-  // Social sign-in (redirects to provider login page)
-  const handleProviderLogin = async (providerId) => {
-    try {
-      await signInWithRedirect({ provider: providerId });
-    } catch (err) {
-      console.error("Error signing in with provider:", providerId, err);
-    }
-  };
-
-  // Guest login
-  const handleGuestLogin = async () => {
-    try {
-      const user = await signInAnonymously();
-      console.log("Signed in as guest:", user);
-    } catch (err) {
-      console.error("Error with guest login:", err);
-    }
-  };
-
-  // Email signup (you can replace with Amplify email/password auth later)
-  const handleEmailSignUp = (e) => {
-    e.preventDefault();
-    alert("Signed up with email!");
-  };
-};
-
-export default SignInRegister;
