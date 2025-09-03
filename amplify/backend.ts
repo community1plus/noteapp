2a438bd6009fdbfdad6d72b20bbfc3e8436a6c0c
Ade Oloyede <ade-oloyede@hotmail.com>
2025-09-03 13:25:40 +1000

    Remove Google & Facebook IdPs from Amplify auth

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,7 +1,5 @@
 import { defineBackend } from "@aws-amplify/backend";
 import { auth } from './auth/resource';
-import { storage } from './storage/resource';
-
 import { defineStorage } from '@aws-amplify/backend';
 
 export const storage = defineStorage({
