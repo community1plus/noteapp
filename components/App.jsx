0a0ffc27e5d728d0e30023e562473a6208b2bc8e
Ade Oloyede <ade-oloyede@hotmail.com>
2025-09-11 21:07:43 +1000

    Remove Google & Facebook IdPs from Amplify auth

--- components/App.jsx
+++ components/App.jsx
@@ -3,7 +3,6 @@ import { withAuthenticator, Authenticator } from '@aws-amplify/ui-react';
 import CommunityPlusLandingPage from "./CommunityPlusLandingPage";
 import CommunityPlusHome from "./CommunityPlusHome";
 import SignInRegister from "../src/SignInRegister";
-
 // Wrap your protected page with the HOC
 const ProtectedCommunityPlusHome = CommunityPlusHome;
 
@@ -15,22 +14,20 @@ function App() {
         <Route path="/" element={<CommunityPlusLandingPage />} />
 
         {/* Protected route */}
-        
-        <Route
-  path="/home"
-  element={
-    <Authenticator>
-      {({ signOut, user }) => (
-        <ProtectedCommunityPlusHome user={user} signOut={signOut} />
-      )}
-    </Authenticator>
-  }
-/>
+        <Route path="/home"
+          element={
+            <Authenticator>
+              {({ signOut, user }) => (
+                <ProtectedCommunityPlusHome user={user} signOut={signOut} />
+              )}
+            </Authenticator>
+          }
+        />
 
        <Route path="/auth" element={<SignInRegister />} />
       </Routes>
     </BrowserRouter>
   );
 }
-
+  
 export default App;
