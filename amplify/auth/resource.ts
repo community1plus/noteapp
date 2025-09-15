b65188d9d0ee61a4c22aee4c793163ca26a21da8
Ade Oloyede <ade-oloyede@hotmail.com>
2025-09-15 11:36:10 +1000

    Remove Google & Facebook IdPs from Amplify auth

--- amplify/auth/resource.ts
+++ amplify/auth/resource.ts
@@ -8,12 +8,12 @@ export const auth = defineAuth({
       google: {
         clientId: secret("GOOGLE_CLIENT_ID"),
         clientSecret: secret("GOOGLE_CLIENT_SECRET"),
-        scopes: ['openid','email'],
+        scopes: ['openid','email', 'profile'],
       },
       facebook: {
         clientId: secret("FACEBOOK_APP_ID"),
         clientSecret: secret("FACEBOOK_APP_SECRET"),
-        scopes: ['openid','email'],
+        scopes: ['openid', 'email', 'profile']
       },
       callbackUrls: [
         "http://localhost:3000/",
