6d115cc6775674c8515ba7eec4544ef7d5a29ffd
Ade Oloyede <ade-oloyede@hotmail.com>
2025-09-29 15:38:07 +1000

    Remove Google & Facebook IdPs from Amplify auth

--- components/CommunityPlusDashboard.jsx
+++ components/CommunityPlusDashboard.jsx
@@ -80,7 +80,7 @@ function CommunityPlusDashboard() {
 
       {/* Right column: Feed */}
       <div className="feed-column">
-        <div>Community News & Events</div>
+        <div>Community News </div>
         <div>{CommunityPlusFetchfbPosts}</div>
       </div>
     </div>
