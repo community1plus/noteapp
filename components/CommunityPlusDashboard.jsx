895d38c049209725f1852bd24179cf3efcac145d
community1plus@gmail.com <community1plus@gmail.com>
2025-10-03 15:45:11 +1000

    Remove Google & Facebook IdPs from Amplify auth

--- components/CommunityPlusDashboard.jsx
+++ components/CommunityPlusDashboard.jsx
@@ -52,18 +52,7 @@ function CommunityPlusDashboard() {
           googleMapsApiKey="AIzaSyCPG5QI1XTpFjgcTaDoY_rN5qxR3susJrc"
           libraries={["places"]}
         >
-          <div className="search-bar">
-            <StandaloneSearchBox
-              onLoad={(ref) => (searchBoxRef.current = ref)}
-              onPlacesChanged={onPlacesChanged}
-            >
-              <input
-                type="text"
-                placeholder="Enter Address,suburb or postcode"
-                className="location-input"
-              />
-            </StandaloneSearchBox>
-          </div>
+          
           <GoogleMap
               center={coords}
               zoom={14}
