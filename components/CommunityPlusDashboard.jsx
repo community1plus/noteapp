c90eaacec10c6946e8621192a15dd81b786cfc97
community1plus@gmail.com <community1plus@gmail.com>
2026-01-03 10:16:35 +1100

    Remove Google & Facebook IdPs from Amplify auth

--- components/CommunityPlusDashboard.jsx
+++ components/CommunityPlusDashboard.jsx
@@ -4,8 +4,6 @@ import "../src/CommunityPlusDashboard.css";
 import CommunityPlusFetchfbPosts from "./CommunityPlusFetchfbPosts";
 import CommunityPlusSideBar from "./CommunityPlusSideBar";
 
-
-
 function CommunityPlusDashboard() {
   const [coords, setCoords] = useState({ lat: -37.8136, lng: 144.9631 }); // default Melbourne
   const [location, setLocation] = useState("Detecting location...");
